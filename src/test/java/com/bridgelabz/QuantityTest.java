26b8790504703c60dda457541cccb367a4002306
sushant2931 <sushantlad8888@gmail.com>
2021-11-10 01:59:45 +0530

    ADDED : 1 Inch and 1 Yard Should Not Equal

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -91,4 +91,11 @@ public class QuantityTest {
         Assertions.assertFalse(compareCheck);
     }
 
+    @Test
+    public void given1InchAnd1Yard_WhenCompared_ShouldReturnFalse() {
+        Length inch = new Length(Length.Unit.INCH, 1.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        boolean compareCheck = inch.compare(yard);
+        Assertions.assertFalse(compareCheck);
+    }
 }
