9636e1b431d37e0bd796a87083fe2da2e066d58f
sushant2931 <sushantlad8888@gmail.com>
2021-11-09 17:52:34 +0530

    ADDED : Test Case To Check Equality 0 Feet and 1 Feet

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -10,4 +10,11 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotSame(feet1, feet2);
+    }
 }
