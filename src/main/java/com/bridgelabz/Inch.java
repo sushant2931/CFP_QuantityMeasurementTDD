73451f8ea5ae1f85fa9fbae804d52103200b624d
sushant2931 <sushantlad8888@gmail.com>
2021-11-09 18:23:35 +0530

    ADDED : Test Case Passed By USing Equality Method

--- src/main/java/com/bridgelabz/Inch.java
+++ src/main/java/com/bridgelabz/Inch.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Objects;
+
 public class Inch {
     private double value;
 
     public Inch(double value) {
         this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inch inch = (Inch) o;
+        return Double.compare(inch.value, value) == 0;
+    }
+
 }
