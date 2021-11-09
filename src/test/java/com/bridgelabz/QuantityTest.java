188396cc3b903b91cf528c7d9f4644f15f9409dc
sushant2931 <sushantlad8888@gmail.com>
2021-11-10 01:57:49 +0530

    ADDED : 1 Feet i Not Equal TO 1 Yard

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -83,5 +83,12 @@ public class QuantityTest {
         Assertions.assertTrue(compareCheck);
     }
 
+    @Test
+    public void given1FeetAnd1Yard_WhenCompared_ShouldReturnFalse() {
+        Length feet = new Length(Length.Unit.FEET, 1.0);
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        boolean compareCheck = feet.compare(yard);
+        Assertions.assertFalse(compareCheck);
+    }
 
 }
