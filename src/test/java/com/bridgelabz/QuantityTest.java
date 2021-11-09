a12a34109662d2fe5fd790f8c514fcc4de23beff
sushant2931 <sushantlad8888@gmail.com>
2021-11-09 18:34:08 +0530

    ADDED : Test Case For Check Different Type

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -66,4 +66,11 @@ public class QuantityTest {
         boolean result = inch1 == inch1;
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given0Inchand0InchFromDifferentType_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0);
+        Assert.assertNotSame(inch1, inch2);
+    }
 }
