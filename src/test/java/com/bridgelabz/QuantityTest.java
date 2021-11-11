8e0bbc4fec48c92e96d69e691b3976dc2e808b5d
sushant2931 <sushantlad8888@gmail.com>
2021-11-11 18:54:43 +0530

    ADDED : test Case And Method For Compare 1000 Ml and 1 Litre  Should Return Equal

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -256,5 +256,12 @@ public class QuantityTest {
         Assertions.assertTrue(compareCheck);
     }
 
+    @Test
+    public void given1000MilliLiterAnd1Liter_WhenCompared_ShouldReturnEqualVolume() {
+        Volume ml = new Volume(Volume.Unit.MILLI_LITER, 1000.0);
+        Volume liter = new Volume(Volume.Unit.LITER, 1.0);
+        boolean compareCheck = ml.compare(liter);
+        Assertions.assertTrue(compareCheck);
+    }
 
 }
